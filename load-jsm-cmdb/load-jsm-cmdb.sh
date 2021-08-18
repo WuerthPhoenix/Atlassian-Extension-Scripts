4be7a0db3dd0b04189cad5d72e9ca9add4025a69
Nicola <nicola.degara@gmail.com>
2021-08-18 16:37:15 +0200

    Update load-jsm-cmdb.sh
    Added License Disclaimer

--- load-jsm-cmdb/load-jsm-cmdb.sh
+++ load-jsm-cmdb/load-jsm-cmdb.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#"-------------------------------------------------------------------------------------------"
+#">> LICENSED UNDER GNU General Public License, v3  (see LICENSE.md for details)            |"
+#" | Atlassian-Extension-Scripts Copyright (C) 2021  Würth Phoenix                          |"
+#" | This program comes with ABSOLUTELY NO WARRANTY.                                        |"
+#" | This is free software, and you are welcome to redistribute it under certain conditions;|"
+#"-------------------------------------------------------------------------------------------"
+
 loc=$(dirname "$0")
 me=`basename "$0"`
 source $loc/jira_variables.sh $me $loc
